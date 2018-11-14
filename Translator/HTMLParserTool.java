5b6eee6a2f2cde2930643f1b63ba7052abaab486
Yeabkal <ywubshit@udallas.edu>
2018-11-14 13:14:11 -0600

    Add Queue structure for Code-Text separation

--- Translator/HTMLParserTool.java
+++ Translator/HTMLParserTool.java
@@ -8,6 +8,8 @@ import java.io.PrintWriter;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Random;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class HTMLParserTool {
 
@@ -24,15 +26,15 @@ public class HTMLParserTool {
 		this.destFile = destFile;
 	}
 
-	public void convert(String txt, String dest) throws Exception {
+	public void convert() throws Exception {
 		Translator translator = new Translator();
-		PrintWriter writer = new PrintWriter(dest);
+		PrintWriter writer = new PrintWriter(destFile);
 		int ind = 1;
 		StringBuilder toTrans = new StringBuilder();
 		StringBuilder code = new StringBuilder("<");
 		int state = 1;
-		while(ind < txt.length()) {
-			char current = txt.charAt(ind);
+		while(ind < sourceCode.length()) {
+			char current = sourceCode.charAt(ind);
 			if(current == '<') {
 				String textToTranslate = toTrans.toString();
 				if(!textToTranslate.trim().equals("")) {
@@ -57,6 +59,66 @@ public class HTMLParserTool {
 		writer.print(code.toString());
 		writer.close();
 	}
+
+	public Queue<String>[] convertToTranslatableFormat() throws Exception {
+		
+		Queue<String> htmlCodeList = new LinkedList<>();
+		Queue<String> wordsList = new LinkedList<>();
+		
+		Translator translator = new Translator();
+		PrintWriter writer = new PrintWriter(destFile);
+		int ind = 1;
+		
+		StringBuilder toTrans = new StringBuilder();
+		StringBuilder htmlCode = new StringBuilder();
+		
+		htmlCode.append("<");
+
+		int state = 1;
+		
+		
+		while(ind < sourceCode.length()) {
+			char current = sourceCode.charAt(ind);
+			if(current == '<') {
+				String textToTranslate = toTrans.toString();
+				if(!textToTranslate.trim().equals("")) {
+					wordsList.add(textToTranslate);
+					toTrans = new StringBuilder();
+				}
+				htmlCode.append("\n<");
+				state++;
+			} else if (current == '>') {
+				htmlCode.append(current);
+				if(ind < sourceCode.length() - 1) {
+					if(sourceCode.charAt(ind + 1) != '<') {
+						String code = htmlCode.toString();
+						htmlCodeList.add(code);
+						htmlCode = new StringBuilder();
+					}
+				}
+				state--;
+			} else {
+				if (state == 0) {
+					toTrans.append(current);
+				} else {
+					htmlCode.append(current);
+				}
+			}
+			ind++;
+		}
+		while(!htmlCodeList.isEmpty() && !wordsList.isEmpty()) {
+			writer.print(htmlCodeList.poll());
+			writer.print(wordsList.poll());
+		}
+		while(!htmlCodeList.isEmpty()) {
+			writer.print(htmlCodeList.poll());
+		}
+		writer.close();
+		Queue[] ret = new Queue[2];
+		ret[0] = htmlCodeList;
+		ret[1] = wordsList;
+		return ret;
+	}
 	
 	public void run() throws Exception {
 		FileExtractor extractor = new FileExtractor(tempFile);
@@ -64,10 +126,10 @@ public class HTMLParserTool {
 		
 		fetcher.saveSourceCodeToFile();
 		sourceCode = extractor.readFile(tempFile);
-		convert(sourceCode, destFile);
+		convert();
 	}
 
-/*	public static void main(String[] args) throws Exception {
-		new HTMLParserTool("https://projecteuler.net/archives", "temp", "new22.html").run();
-	}*/
+	public static void main(String[] args) throws Exception {
+		new HTMLParserTool("https://projecteuler.net/archives", "temp", "new4.html").run();
+	}
 }
